000f4563c244ed7bd5ffcf237b52f1348a858ad6
Bryan R Moreno Duran <63120377+morenocode@users.noreply.github.com>
2023-10-24 19:08:27 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".swiper-hero", {
+    // Optional parameters
+    //slidesPerView: "auto",
+    //spaceBetween: 15,
+    // slidesPerGroupAuto: true,
+
+    direction: "horizontal",
+    loop: true,
+    // allowTouchMove: true,
+    // effect: "cube",
+    autoplay: {
+      delay: 2000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      // type: "progressbar"
+      clickable: true,
+      // dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    //   draggable: true,
+    // },
+  });
